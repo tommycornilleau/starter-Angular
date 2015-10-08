4857c3530835ce0e02306e1152cbc1650772c0e2
tommycor <tommycornilleau@gmail.com>
2015-10-08 11:31:32 +0200

    last commit before longtime, project getting to specific to work in this general repo

--- app/js/app.js
+++ app/js/app.js
@@ -4,10 +4,11 @@ var angular = angular;
 
 angular.module('app', ['ngRoute', 'main']).
 config(function($routeProvider) {
-	$routeProvider.when('/projects', { templateUrl: 'partials/projects.html' });
-	$routeProvider.when('/project/:name', { templateUrl: 'partials/single.html', controller: 'WorkoutController' });
-	$routeProvider.when('/about', { templateUrl: 'partials/about.html' });
-	$routeProvider.otherwise({ redirectTo: '/start' });
+	$routeProvider.when('/', { templateUrl: 'partials/home.html', controller: 'homeController' });
+	$routeProvider.when('/projects', { templateUrl: 'partials/projects.html', controller: 'projectsController' });
+	$routeProvider.when('/project/:name', { templateUrl: 'partials/single.html', controller: 'singleController' });
+	$routeProvider.when('/about', { templateUrl: 'partials/about.html', controller: 'aboutController' });
+	$routeProvider.otherwise({ redirectTo: '/' });
 });
 
 angular.module('main', []);
